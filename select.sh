8706709f8eeee3f82d4d8e934c58f1c3a183f0f0
tamilselvanpalani <44170732+tamilselvanpalani@users.noreply.github.com>
2018-10-28 15:50:45 +0530

    Update select.sh

--- select.sh
+++ select.sh
@@ -1,1 +1,8 @@
-
+#!/bin/sh
+FILE=/root/hive_scripts/select.properties
+db_name=$(grep -i 'databaseName' $FILE  | cut -f2 -d'=')
+table_name=$(grep -i 'tableName' $FILE  | cut -f2 -d'=')
+echo "connecting to database - " $db_name
+echo  "using table - " $table_name
+hive -hiveconf DB_NAME=$db_name -hiveconf TABLE_NAME=$table_name -S -f /root/hive_scripts/select.hql | sed 's/[\t]/,/g' > /root/data_out/iris_`date '+%Y-%m-%d':'%H%M%S'`.csv
+echo "hive query result is available ---> /root/data_out/"
